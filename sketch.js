0ccb4226e0da0b6dbb0f5d266b60843c903f4fe5
sreevardhan37 <74353232+sreevardhan37@users.noreply.github.com>
2021-03-03 19:05:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var player1,player2,player3,player4;
+
+var track;
+
+function preload(){
+  track = loadImage("../images/track.jpg");
+    ground = loadImage("../images/ground.png");
+}
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+  if(gameState === 2){
+    game.end();
+  }
+}
